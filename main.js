574c37f06f5ff2df56a556bb8cb7920363c65699
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2022-01-02 18:43:30 -0500

    Refactors functions to ES6 syntax

--- main.js
+++ main.js
@@ -1,55 +1,70 @@
-import './style.css';
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
 
-import vertexShader from './shaders/vertexShader.glsl'
-import fragmentShader from './shaders/fragmentShader.glsl'
+import vertexShader from "./shaders/vertexShader.glsl";
+import fragmentShader from "./shaders/fragmentShader.glsl";
 
 // Setup
 let meshRay;
 let mouseMoved = false;
-var video;
+const video = document.getElementById("video");
+
+const initWebcam = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => console.log("An error occured! " + err));
+};
 
 initWebcam();
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(10);
 camera.position.setY(0);
-camera.lookAt(0,0,0);
-
-
+camera.lookAt(0, 0, 0);
 
 renderer.render(scene, camera);
 
 const textureLoader = new THREE.TextureLoader();
 
-const texture = new THREE.VideoTexture( video );
+const texture = new THREE.VideoTexture(video);
 
-const geometry = new THREE.PlaneGeometry( 50, 25, 1000, 1000 );
+const geometry = new THREE.PlaneGeometry(50, 25, 1000, 1000);
 const material = new THREE.ShaderMaterial({
-  uniforms: { 
+  uniforms: {
     uTime: { type: "f", value: 0 },
-    uMouse: { type: "v2", value: new THREE.Vector2()},
-    uResolution: { type: "v2", value: new THREE.Vector2()},
+    uMouse: { type: "v2", value: new THREE.Vector2() },
+    uResolution: { type: "v2", value: new THREE.Vector2() },
     texture1: { type: "t", value: texture },
-    texture2: { type: "t", value: textureLoader.load( 'noise.png' ) },
+    texture2: { type: "t", value: textureLoader.load("noise.png") },
   },
   vertexShader,
-  fragmentShader
+  fragmentShader,
 });
 
-material.uniforms.texture1.value.wrapS = material.uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
-material.uniforms.texture2.value.wrapS = material.uniforms.texture2.value.wrapT = THREE.RepeatWrapping;
+material.uniforms.texture1.value.wrapS =
+  material.uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
+material.uniforms.texture2.value.wrapS =
+  material.uniforms.texture2.value.wrapT = THREE.RepeatWrapping;
 
-const plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
@@ -57,122 +72,106 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const mouse = new THREE.Vector2( 0, 0 );
+const mouse = new THREE.Vector2(0, 0);
 const raycaster = new THREE.Raycaster();
 
 // THREE.Mesh just for mouse raycasting
-const geometryRay = new THREE.PlaneGeometry( 100, 25, 100, 100 );
-meshRay = new THREE.Mesh( geometryRay, new THREE.MeshBasicMaterial( { color: 0xFFFFFF, visible: false } ) );
+const geometryRay = new THREE.PlaneGeometry(100, 25, 100, 100);
+meshRay = new THREE.Mesh(
+  geometryRay,
+  new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
+);
 meshRay.matrixAutoUpdate = false;
 meshRay.updateMatrix();
-scene.add( meshRay );
+scene.add(meshRay);
 
-document.addEventListener( 'mousemove', onDocumentMouseMove );
-document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
-document.addEventListener( 'keydown', onKeyPress);
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize(){
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function onDocumentMouseMove( event ) {
-
-  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  mouse.y = -( event.clientY / window.innerHeight ) * 2  + 1;
+const onDocumentMouseMove = (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   mouseMoved = true;
+};
 
-}
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-function onDocumentMouseWheel( event ) {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
 
-  camera.position.z += event.deltaY/500;
-
-}
-
-function initWebcam() {
-
-	video = document.getElementById('video');
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-    })
-    .catch(function(err) {
-        console.log("An error occured! " + err);
-    });
-
-}
-
-
-function onKeyPress(e) {
-
-    var elem = document.documentElement;
-
-    console.log(e.keyCode);
-    if(e.keyCode == 70) {
-      openFullscreen(elem);  
-    }
-    if(e.keyCode == 27) {
-      closeFullscreen()
-    }
-}
-
+const onDocumentMouseWheel = (event) => {
+  camera.position.z += event.deltaY / 500;
+};
 
 /* View in fullscreen */
-function openFullscreen(element) {
-
+const openFullscreen = (element) => {
   // Supports most browsers and their versions.
-  var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+  var requestMethod =
+    element.requestFullScreen ||
+    element.webkitRequestFullScreen ||
+    element.mozRequestFullScreen ||
+    element.msRequestFullScreen;
 
-  if (requestMethod) { // Native full screen.
-      requestMethod.call(element);
-  } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-      var wscript = new ActiveXObject("WScript.Shell");
-      if (wscript !== null) {
-          wscript.SendKeys("{F11}");
-      }
+  if (requestMethod) {
+    // Native full screen.
+    requestMethod.call(element);
+  } else if (typeof window.ActiveXObject !== "undefined") {
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
   }
-}
+};
 
 /* Close fullscreen */
-function closeFullscreen() {
+const closeFullscreen = () => {
   if (document.exitFullscreen) {
     document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
     document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
     document.msExitFullscreen();
   }
-}
+};
 
-function animate() {
+const onKeyPress = (e) => {
+  var elem = document.documentElement;
+
+  console.log(e.keyCode);
+  if (e.keyCode == 70) {
+    openFullscreen(elem);
+  }
+  if (e.keyCode == 27) {
+    closeFullscreen();
+  }
+};
+
+document.addEventListener("mousemove", onDocumentMouseMove);
+document.addEventListener("mousewheel", onDocumentMouseWheel, false);
+document.addEventListener("keydown", onKeyPress);
+
+window.addEventListener("resize", onWindowResize, false);
+
+const animate = () => {
   requestAnimationFrame(animate);
 
   material.uniforms.uTime.value += 0.01;
 
-  raycaster.setFromCamera( mouse, camera );
-  const intersects = raycaster.intersectObject( meshRay );
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(meshRay);
 
-  if ( intersects.length > 0 ) {
-      const point = intersects[ 0 ].point;
-      material.uniforms.uMouse.value.x = point.x;
-      material.uniforms.uMouse.value.y = point.y;
-  }
-  else {
+  if (intersects.length > 0) {
+    const point = intersects[0].point;
+    material.uniforms.uMouse.value.x = point.x;
+    material.uniforms.uMouse.value.y = point.y;
+  } else {
     material.uniforms.uMouse.value.x = 10000;
     material.uniforms.uMouse.value.y = 10000;
   }
 
-
-
   renderer.render(scene, camera);
-}
+};
 
 animate();
