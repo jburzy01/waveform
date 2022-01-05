4ce36638a8abd98397d69419203edc4b486a7c39
Jackson Burzynski <jacksonburzynski@gmail.com>
2022-01-05 15:37:33 -0500

    update package-lock and remove comments

--- types/glsl.d.ts
+++ types/glsl.d.ts
@@ -1,7 +1,4 @@
-// import { ShaderMaterialParameters } from "three/src/materials/ShaderMaterial" 
-
 declare module '*.glsl' {
-  // const value: ShaderMaterialParameters["vertexShader"]
   const value: string;
   export default value;
 }
